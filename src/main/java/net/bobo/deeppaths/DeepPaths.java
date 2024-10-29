4ab753447f83acc4fd0d05b32bc6b448c5e3d35d
BuildTools <unconfigured@null.spigotmc.org>
2024-10-29 14:07:00 +0100

    Initial commit

---
+++ src/main/java/net/bobo/deeppaths/DeepPaths.java
@@ -0,0 +1,17 @@
+package net.bobo.deeppaths;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeepPaths implements ModInitializer {
+
+	public static final String MOD_ID = "deep-paths";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bobo/deeppaths/DeepPathsClient.java
@@ -0,0 +1,10 @@
+package net.bobo.deeppaths;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeepPathsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
